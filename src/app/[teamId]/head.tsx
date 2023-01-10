5ba0dcecc959a0f8807c85b396005e4f3416f1fc
José Aular <jose14aular@gmail>
2023-01-10 13:35:27 -0400

    Add title

--- src/app/[teamId]/head.tsx
+++ src/app/[teamId]/head.tsx
@@ -9,7 +9,7 @@ export default async function Head({ params }: { params: { teamId: string } }) {
 
   return (
     <>
-      <title>NHL Scores & Schedules</title>
+      <title>{title}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Like ESPN, but streamlined." />
       <link rel="icon" href="/favicon.ico" />
